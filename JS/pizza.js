a50f5baf54d45ca4ac0cfe865ffa35b7a1e9f9d4
Francisco Mateus <gtaskfm@gmail.com>
2017-09-05 19:45:09 +0100

    Update pizza.js

--- JS/pizza.js
+++ JS/pizza.js
@@ -10,7 +10,16 @@ const getSubTotal = (itemCount) => {
   return itemCount * 7.5;
 };
 
+const getTax = () => {
+ return getSubTotal(orderCount) * 0.06;
+}
+
+const getTotal = () => {
+  return getSubTotal(orderCount) + getTax();
+}
+
 takeOrder('mushroom', 'thin crust');
 takeOrder('spinach', 'whole wheat');
 takeOrder('pepperoni', 'brooklyn style');
 console.log(getSubTotal(orderCount));
+console.log(getTotal());
