232ca5f83f7cb6e1cbe4aab3767b42ba23579668
Francisco Mateus <gtaskfm@gmail.com>
2017-09-05 19:37:57 +0100

    Update
    counter and return

--- JS/pizza.js
+++ JS/pizza.js
@@ -1,3 +1,16 @@
-const takeOrder = () => {
-  console.log("Order: pizza");
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount++
+  
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
 };
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+console.log(getSubTotal(orderCount));
